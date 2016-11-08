0e6e21e6f07de4ebd51aaf7ab3198d4732a7f0f6
Michael Freese <mrfreese1@gmail.com>
2016-11-08 15:42:12 -0500

    avatar added

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ $('.chirps').html('<table class="table table-striped"></table>')
 $('.chirps table').append('<tr><th>User</th><th>Time</th><th>Avatar</th><th>Chirp</th></tr>')
 console.log(r)
 r.posts.forEach(function(c){
-  $('.chirps table').append('<tr><td>' + c.users + '</td><td>' + c.time + '</td><td>' + c.avatar + '</td><td>' + c.chirp + '</td></tr>')
+  if (c.user.avatar == null){
+    c.user.avatar = 'https://robohash.org/' + c.user.id + '?size=60x60'
+  }
+  $('.chirps table').append('<tr><td>' + c.user.name + '</td><td>' + c.created_at + '</td><td><img src=' + c.user.avatar + '></td><td>' + c.chirp + '</td></tr>')
 })
 })

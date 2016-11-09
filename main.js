365c68abeb7415674fc7d75dce4c1713a33a9acf
Michael Freese <mrfreese1@gmail.com>
2016-11-08 21:55:05 -0500

    explorer

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $.getJSON('https://nameless-cove-75673.herokuapp.com/posts')
 .then(function(r){
-$('.chirps').html('<table class="table table-striped"></table>')
-$('.chirps table').append('<tr><th>User</th><th>Time</th><th>Avatar</th><th>Chirp</th></tr>')
+$('.chirps').html('<table class="table table-bordered">')
+$('.chirps table').append('<tr><th>Avatar</th><th>User</th><th>Chirp</th><th>Time Posted</th></tr>')
 console.log(r)
 r.posts.forEach(function(c){
   if (c.user.avatar == null){
-    c.user.avatar = 'https://robohash.org/' + c.user.id + '?size=60x60'
+    c.user.avatar = 'https://robohash.org/' + c.user.id + '?size=55x55'
   }
-  $('.chirps table').append('<tr><td>' + c.user.name + '</td><td>' + c.created_at + '</td><td><img src=' + c.user.avatar + '></td><td>' + c.chirp + '</td></tr>')
+  $('.chirps table').append('<tr><td><img src=' + c.user.avatar + '></td><td>' + c.user.name + '</td><td>' + c.chirp + '></td><td>' + c.created_at + '</td></tr>')
 })
 })
